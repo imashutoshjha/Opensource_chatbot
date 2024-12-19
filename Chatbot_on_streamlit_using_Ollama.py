5d87169fccedf5eca4a86cbf3971f873e515c39b
Ashutosh jha <49037490+imashutoshjha@users.noreply.github.com>
2024-12-19 19:51:22 +0530

    Update1_Chatbot_on_streamlit_using_Ollama.py

--- Chatbot_on_streamlit_using_Ollama.py
+++ Chatbot_on_streamlit_using_Ollama.py
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"]=st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With Ollama"
 
